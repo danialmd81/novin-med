b537ffdf70cde629e4f1f81600a6f29cf99d5994
Danial Mobini <danialmobinidh81@gmail.com>
2025-12-11 00:59:23 +0330

    Add start-image.sh script for Raspberry Pi virtual machine setup

---
+++ start-image.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+virt-install \
+  --name pi \
+  --os-variant linux2020 \
+  --arch armv6l \
+  --machine versatilepb \
+  --cpu arm1176 \
+  --vcpus 1 \
+  --memory 256 \
+  --import \
+  --disk 2018-11-13-raspbian-stretch.img,format=raw,bus=virtio \
+  --network user,model=virtio \
+  --video vga \
+  --graphics spice \
+  --rng device=/dev/urandom,model=virtio \
+  --boot 'dtb=qemu-rpi-kernel/versatile-pb-buster.dtb,kernel=qemu-rpi-kernel/kernel-qemu-4.19.50-buster,kernel_args=root=/dev/vda2 panic=1' \
+  --events on_reboot=destroy
