db20e2638e738dad9947de1cdb03236a014858e4
Danial Mobini <danialmobinidh81@gmail.com>
2025-12-13 16:03:41 +0330

    Fix drive path and network device configuration in start-image.sh for Raspberry Pi QEMU setup

--- projects/project1/rpi-qemu/start-image.sh
+++ projects/project1/rpi-qemu/start-image.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 qemu-system-arm \
-  -M versatilepb \
-  -cpu arm1176 \
-  -m 256 \
-  -drive file=../../../2018-11-13-raspbian-stretch.img,format=raw \
-  -net user,hostfwd=tcp::5022-:22 \
-  -dtb versatile-pb-buster.dtb \
-  -kernel kernel-qemu-4.19.50-buster \
-  -append 'root=/dev/sda2 panic=1' \
-  -device rtl8139
+	-M versatilepb \
+	-cpu arm1176 \
+	-m 256 \
+	-drive file=/home/danial/Code/svn/trunk/OS/Images/2018-11-13-raspbian-stretch.img,format=raw \
+	-net nic,model=smc91c111 \
+	-net user,hostfwd=tcp::5022-:22 \
+	-dtb versatile-pb.dtb \
+	-kernel kernel-qemu-4.14.79-stretch \
+	-append 'root=/dev/sda2 panic=1'
